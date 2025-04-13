66ff37dcdee263a34e90183f9a65c62b132e89e5
Nikhil Tumbde <nikhiltumbde.unofficial@gmail.com>
2025-04-13 13:27:36 +0530

    added main to tests, cleaned nanograd code

--- tests.py
+++ tests.py
@@ -283,5 +283,5 @@ def run_all_tests():
     test_transpose()
     print("All tests passed successfully!")
 
-# if __name__ == '__main__':
-#     run_all_tests()
+if __name__ == '__main__':
+    run_all_tests()
